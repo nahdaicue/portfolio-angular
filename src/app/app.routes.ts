da42e8ea44be45af32cd7e3b8429739f4e5e9397
nahdaicue <nahdaicue@gmail.com>
2025-12-17 09:03:22 -0300

    create generic project component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'projects', component: ProjectsComponent }
 ];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
+import { ProjectsComponent } from "../projects/projects.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AboutComponent],
+  imports: [AboutComponent, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
