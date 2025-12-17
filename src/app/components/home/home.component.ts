079a24099d552121d3bc121d92b6f077524b13e2
nahdaicue <nahdaicue@gmail.com>
2025-12-17 09:15:31 -0300

    create contact component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
 import { ProjectsComponent } from "../projects/projects.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AboutComponent, ProjectsComponent],
+  imports: [AboutComponent, ProjectsComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
