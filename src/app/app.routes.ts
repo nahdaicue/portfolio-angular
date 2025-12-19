c244bcbe841f6b42036e1f24d4e3dc9481464816
nahdaicue <nahdaicue@gmail.com>
2025-12-19 16:04:34 -0300

    create logIn component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ErrorComponent } from './components/error/error.component';
+import { LogInComponent } from './components/log-in/log-in.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'projects', component: ProjectsComponent },
     { path: 'contact', component: ContactComponent },
+    { path: 'login', component: LogInComponent },
     { path: '**', component: ErrorComponent }
 ];
