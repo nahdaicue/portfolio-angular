53b232d1ccf87c266256fc2fb4eb29c06ae057e2
nahdaicue <nahdaicue@gmail.com>
2026-07-29 18:08:47 -0300

    add log out

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  private authService = inject(AuthService);
+
+  loggedIn$ = this.authService.loggedIn$;
 
   isMenuOpen = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
-  
+
+  logout() {
+    this.authService.logout();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,22 +2,31 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-
   private readonly TOKEN_KEY = 'auth_token';
   private readonly API_URL = 'http://localhost:8080';
 
   private http = inject(HttpClient);
   private router = inject(Router);
 
+  private loggedIn = new BehaviorSubject<boolean>(this.isLoggedIn());
+
+  loggedIn$ = this.loggedIn.asObservable();
+
   login(userName: string, password: string) {
-    return this.http.post<{ token: string }>(`${this.API_URL}/api/auth/login`, { userName, password })
-      .pipe(
-        tap(response => this.saveToken(response.token))
-      );
-  }
+  return this.http.post<{ token: string }>(
+    `${this.API_URL}/api/auth/login`,
+    { userName, password }
+  ).pipe(
+    tap(response => {
+      this.saveToken(response.token);
+      this.loggedIn.next(true);
+    })
+  );
+}
 
   saveToken(token: string): void {
     localStorage.setItem(this.TOKEN_KEY, token);
@@ -27,10 +36,11 @@ export class AuthService {
     return localStorage.getItem(this.TOKEN_KEY);
   }
 
-  logout(): void {
-    localStorage.removeItem(this.TOKEN_KEY);
-    this.router.navigate(['/login']);
-  }
+ logout(): void {
+  localStorage.removeItem(this.TOKEN_KEY);
+  this.loggedIn.next(false);
+  this.router.navigate(['/login']);
+}
 
   isLoggedIn(): boolean {
     return !!this.getToken();
